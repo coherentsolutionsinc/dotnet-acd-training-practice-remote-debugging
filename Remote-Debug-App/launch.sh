b8851ca5ab4b9286230b70a47827109c4ffc81bd
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2025-12-04 19:23:25 +0300

    Slightly simplified container image.

--- Remote-Debug-App/launch.sh
+++ Remote-Debug-App/launch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cat >/etc/motd <<EOL 
+cat >/etc/motd <<EOL
   _____                               
   /  _  \ __________ _________   ____  
  /  /_\  \\___   /  |  \_  __ \_/ __ \ 
@@ -9,15 +9,9 @@ cat >/etc/motd <<EOL
 A P P   S E R V I C E   O N   L I N U X
 
 Documentation: http://aka.ms/webapp-linux
-NodeJS quickstart: https://aka.ms/node-qs
-NodeJS Version : `node --version`
 
 EOL
 cat /etc/motd
-
-mkdir "$PM2HOME"
-chmod 777 "$PM2HOME"
-ln -s /home/LogFiles "$PM2HOME"/logs
 
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
